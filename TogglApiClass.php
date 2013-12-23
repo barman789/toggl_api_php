79588be16c5623862f99a6abf23c6d0e55f4e36a
barman789 <barman789@gmail.com>
2013-12-23 15:48:40 +0530

    initial changes

---
+++ TogglApiClass.php
@@ -0,0 +1,424 @@
+<?php
+
+/**
+* @author Sandeep <barman789@gmail.com>
+*/
+
+Class TogglApiClass{
+	
+
+/**
+ * This is the api token
+ *
+ * @var string
+ * @access public
+ */
+	public $api_token = '';
+
+/**
+ * This is the response from the API
+ *
+ * @var json
+ * @access public
+ */
+	public $response = '';	
+	
+/**
+ * This is the base URL 
+ *
+ * @var string
+ * @access private
+ */
+	private $_base_url = 'https://www.toggl.com/api/v8/';
+
+/**
+ * Constructor
+ */
+ function __construct( $api_token = null ) {
+        $this->api_token = $api_token;  
+ }
+
+
+/**
+ * does the Authentication and returns User Details
+ *
+ * @return void
+ * @access public
+ */
+  public function doAuthentication( $api_token = null ) { 	
+
+        $url = $this->_base_url . 'me';
+        $this->_callAPI($url);
+ }    
+
+/**
+ * get all Workspaces belonging to the given User
+ *
+ * @return void
+ * @access public
+ */
+  public function getWorkspaces() { 	
+
+        $url = $this->_base_url . 'workspaces';
+        $this->_callAPI($url);
+ }    
+
+/**
+ * get Workspace Detail
+ *
+ * @return void
+ * @access public
+ */
+  public function getWorkspaceDetail( $workspace_id = null ) { 	
+
+        $url = $this->_base_url . 'workspaces/' . $workspace_id;
+        $this->_callAPI($url);
+ }   
+ 
+/**
+ * Edit Workspace
+ *
+ * @return array
+ * @access void
+ */
+  public function editWorkspace( $data = array() ) { 	
+
+        $url = $this->_base_url . 'workspaces/' . $data['id'];
+        $options['data']['workspace'] = $data;
+        $options['put'] = true;
+        $this->_callAPI($url, $options);
+ }
+
+/**
+ * get Workspace Properties
+ *
+ * @return void
+ * @access public
+ */
+  public function getWorkspaceProperties( $workspace_id = null , $property = 'tags') { 	
+
+        $url = $this->_base_url . 'workspaces/' . $workspace_id . '/' . $property;
+        $this->_callAPI($url);
+ }                  
+
+/**
+ * Add a Project to Workspace
+ *
+ * @return void
+ * @access public
+ */
+  public function addProject( $data = array() ) { 	
+
+        $url = $this->_base_url . 'projects';
+        $options['data']['project'] = $data;
+        $options['post'] = true;
+        $this->_callAPI($url, $options);
+ } 
+
+/**
+ * Edit a Project
+ *
+ * @return void
+ * @access public
+ */
+  public function editProject( $data = array() ) { 	
+
+        $url = $this->_base_url . 'projects/' . $data['id'];
+        $options['data']['project'] = $data;
+        $options['put'] = true;
+        $this->_callAPI($url, $options);
+ }
+ 
+/**
+ * get Project Detail
+ *
+ * @return void
+ * @access public
+ */
+  public function getProjectDetail( $project_id = null ) { 	
+
+        $url = $this->_base_url . 'projects/' . $project_id;
+        $this->_callAPI($url);
+ }
+
+/**
+ * get Project Users
+ *
+ * @return void
+ * @access public
+ */
+  public function getProjectUsers( $project_id = null ) { 	
+
+        $url = $this->_base_url . 'projects/' . $project_id . '/project_users';
+        $this->_callAPI($url);
+ }         
+
+/**
+ * Add a Time Entry
+ *
+ * @return void
+ * @access public
+ */
+  public function addTimeEntry( $data = array() ) { 	
+
+        $url = $this->_base_url . 'time_entries';
+        $options['data']['time_entry'] = $data;
+        $options['post'] = true;
+        $this->_callAPI($url, $options);
+ }
+
+/**
+ * Edit a Time Entry
+ *
+ * @return void
+ * @access public
+ */
+  public function editTimeEntry( $data = array() ) { 	
+
+        $url = $this->_base_url . 'time_entries/' . $data['id'];
+        $options['data']['time_entry'] = $data;
+        
+        $options['put'] = true;
+        $this->_callAPI($url, $options);
+ }  
+
+/**
+ * start a Time Entry
+ *
+ * @return void
+ * @access public
+ */
+  public function startTimeEntry( $data = array() ) { 	
+
+        $url = $this->_base_url . 'time_entries/start';
+        $options['data']['time_entry'] = $data;
+        $options['post'] = true;
+        $this->_callAPI($url, $options);
+ } 
+
+
+/**
+ * stop a Time Entry
+ *
+ * @return void
+ * @access public
+ */
+  public function stopTimeEntry( $id = null ) { 	
+
+        $url = $this->_base_url . 'time_entries/'. $id . '/stop';
+        $options['put'] = true;
+        $this->_callAPI($url, $options);
+ }  
+
+/**
+ * Delete a Time Entry
+ *
+ * @return void
+ * @access public
+ */
+  public function deleteTimeEntry( $id ) { 	
+
+        $url = $this->_base_url . 'time_entries/' . $id;
+        $options['delete'] = true;
+        $this->_callAPI($url, $options);
+ }  
+
+ 
+/**
+ * get Time Entry Detail
+ *
+ * @return void
+ * @access public
+ */
+  public function getTimeEntryDetail( $time_entry_id = null ) { 	
+
+        $url = $this->_base_url . 'time_entries/' . $time_entry_id;
+        $this->_callAPI($url);
+ }    
+ 
+/**
+ * Add a client to Workspace
+ *
+ * @return void
+ * @access public
+ */
+  public function addClient( $data = array() ) { 	
+
+        $url = $this->_base_url . 'clients';
+        $options['data']['client'] = $data;
+        $options['post'] = true;
+        $this->_callAPI($url, $options);
+ }
+
+/**
+ * Edit a Client
+ *
+ * @return void
+ * @access public
+ */
+  public function editClient( $data = array() ) { 	
+
+        $url = $this->_base_url . 'clients/' . $data['id'];
+        $options['data']['client'] = $data;
+        $options['put'] = true;
+        $this->_callAPI($url, $options);
+ } 
+ 
+/**
+ * get Client Detail
+ *
+ * @return void
+ * @access public
+ */
+  public function getClientDetail( $client_id = null ) { 	
+
+        $url = $this->_base_url . 'clients/' . $client_id;
+        $this->_callAPI($url);
+ }   
+
+/**
+ * Delete a Client
+ *
+ * @return void
+ * @access public
+ */
+  public function deleteClient( $id ) { 	
+
+        $url = $this->_base_url . 'clients/' . $id;
+        $options['delete'] = true;
+        $this->_callAPI($url, $options);
+ } 
+ 
+/**
+ * get Client Projects
+ *
+ * @return void
+ * @access public
+ */
+  public function getClientProjects( $client_id = null ) { 	
+
+        $url = $this->_base_url . 'clients/' . $client_id . '/projects';
+        $this->_callAPI($url);
+ }    
+
+/**
+ * Add a Tag to Workspace
+ *
+ * @return void
+ * @access public
+ */
+  public function addTag( $data = array() ) { 	
+
+        $url = $this->_base_url . 'tags';
+        $options['data']['tag'] = $data;
+        $options['post'] = true;
+        $this->_callAPI($url, $options);
+ } 
+
+/**
+ * Edit a Tag
+ *
+ * @return void
+ * @access public
+ */
+  public function editTag( $data = array() ) { 	
+
+        $url = $this->_base_url . 'tags/' . $data['id'];
+        $options['data']['tag'] = $data;
+        $options['put'] = true;
+        $this->_callAPI($url, $options);
+ }  
+
+/**
+ * Delete a Tag
+ *
+ * @return void
+ * @access public
+ */
+  public function deleteTag( $id ) { 	
+
+        $url = $this->_base_url . 'tags/' . $id;
+        $options['delete'] = true;
+        $this->_callAPI($url, $options);
+ } 
+ 
+
+/**
+ * get Report
+ *
+ * @return void
+ * @access public
+ */
+  public function getReport( $workspace_id, $options = array() ) { 	
+
+        $defaults = array(
+                       'report_type'           => 'weekly', 
+                       'user_agent'            => 'barman789@gmail.com',
+                       'since'                 => date('c', strtotime('-6 Days')),
+                       'until'                 => date('c'),
+                       'billable'              => 'both',
+                       'order_desc'            => 'on',
+                       'distinct_rates'        => 'off',
+                       'rounding'              => 'off',   
+                       'display_hours'         => 'minutes'
+                    );
+        $options = array_merge($defaults, $options);
+        $url = 'https://toggl.com/reports/api/v2/';
+        $url .= $options['report_type'];
+        unset($options['report_type']);
+        
+        $api_options = array();
+        foreach($options as $key => $option) {
+            $api_options[]= $key.'='.$option;
+        }
+        
+        $query_string = implode('&', $api_options);
+        
+        $url .= '?workspace_id=' . $workspace_id;
+        $url .= '&' . $query_string;
+        
+        echo $url;
+        $this->_callAPI($url);
+ }
+  
+/**
+ * calls the API
+ *
+ * @return void
+ * @access private
+ */
+  private function _callAPI( $url, $options = array() ) { 
+
+        $headers = $this->api_token.':api_token';
+        $ch = curl_init();
+        
+        if(isset($options['data'])) {
+             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($options['data']));
+             curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+        }
+        
+        if(isset($options['put'])) {
+             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        }
+
+        if(isset($options['post'])) {
+             curl_setopt($ch, CURLOPT_POST, true);
+        }        
+        
+        if(isset($options['delete'])) {
+             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+        }        
+        
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC ) ;
+        curl_setopt($ch, CURLOPT_USERPWD, $headers);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);        		
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        
+        $this->response = curl_exec($ch);
+        curl_close($ch);
+  }
+}
+?>
---
+++ usage.php
@@ -0,0 +1,170 @@
+<?php
+
+error_reporting(E_ALL);
+   //FOLLOWING MUST CONTAIN YOUR API TOKEN
+  	$api_token = '';
+  	
+   include_once "TogglApiClass.php";
+  	$togglObj = new TogglApiClass($api_token);
+  	
+  	$apiCall = 'get_report';
+  	
+   switch($apiCall) {
+      case 'authenticate':
+         $togglObj->doAuthentication();
+         break;
+      case 'get_workspaces':
+         $togglObj->getWorkspaces();
+         break;      
+      case 'get_workspace_detail':
+         $id = 482143; //Get Detail of this Workspace
+         $togglObj->getWorkspaceDetail($id);
+         break;  
+      case 'edit_workspace':
+         $data = array();
+         $data['id'] = 482143;    //Workspace id to edit
+         $data['name'] = 'Updated Workspace Name';
+         $togglObj->editWorkspace($data);
+         break;   
+      case 'get_workspace_tags':
+         $id = 482143; //Get Detail of this Workspace
+         $togglObj->getWorkspaceProperties($id, 'tags');
+         break;         
+      case 'get_workspace_tasks':
+         $id = 482143; //Get Detail of this Workspace
+         $togglObj->getWorkspaceProperties($id, 'tasks');
+         break;
+      case 'get_workspace_projects':
+         $id = 482143; //Get Detail of this Workspace
+         $togglObj->getWorkspaceProperties($id, 'projects');
+         break;
+      case 'get_workspace_clients':
+         $id = 482143; //Get Detail of this Workspace
+         $togglObj->getWorkspaceProperties($id, 'clients');
+         break;
+      case 'get_workspace_users':
+         $id = 482143; //Get Detail of this Workspace
+         $togglObj->getWorkspaceProperties($id, 'users');
+         break;                                        
+
+      case 'add_timeEntry':
+         $data = array();
+         $data['wid'] = 482143;  //Workspace Id to add time entry to3794389
+         $data['pid'] = 3794389;  //Project Id to add time entry to
+         $data['start'] = date('c');
+         $data['duration'] = 3600; //in secs
+         $data['description'] = 'Creating API For Toggl'; 
+         $togglObj->addTimeEntry($data);
+         break;
+      case 'start_timeEntry':
+         $data = array();
+         $data['wid'] = 482143;  //Workspace Id to add time entry to3794389
+         $data['pid'] = 3794389;  //Project Id to add time entry to
+         $data['description'] = 'Reading API Docs'; 
+         $togglObj->startTimeEntry($data);
+         break;  
+      case 'stop_timeEntry':
+         $id = 107440835; //Time Entry to Stop
+         $togglObj->stopTimeEntry($id);
+         break;           
+      case 'get_timeEntry_detail':
+         $id = 107440835; //Get Detail of this Time Entry
+         $togglObj->getTimeEntryDetail($id);
+         break;   
+      case 'edit_timeEntry':
+         $data = array();
+         $data['id'] = 107440835; //Time Entry Id to update
+         $data['description'] = 'Reading API Docs';
+         $togglObj->editTimeEntry($data);
+         break;
+      case 'delete_timeEntry':
+         $id = 107440441;
+         $togglObj->deleteTimeEntry($id);
+         break;                                       
+         
+      case 'add_client':
+         $data = array();
+         $data['wid'] = 482143;  //Workspace Id to add client to
+         $data['name'] = 'My Client 2';
+         $togglObj->addClient($data);
+         break;
+      case 'get_client_detail':
+         $id = 10968629; //Get Detail of this Client
+         $togglObj->getClientDetail($id);
+         break;     
+      case 'edit_client':
+         $data = array();
+         $data['id'] = 10968629; //Client Id to update
+         $data['notes'] = 'Very important Client';
+         $togglObj->editClient($data);
+         break;
+      case 'delete_client':
+         $id = 11284510;
+         $togglObj->deleteClient($id);
+         break;             
+      case 'get_client_projects':
+         $id = 10968629; //Get Projects of this Client
+         $togglObj->getClientProjects($id);
+         break;     
+
+      case 'add_project':
+         $data = array();
+         $data['wid'] = 482143;  //Workspace Id to add Project to
+         $data['name'] = 'My Project';
+         $data['cid'] = 10968629;  //Client Id to add Project to (Not Mandatory)
+         $togglObj->addProject($data);
+         break;
+      case 'get_project_detail':
+         $id = 3794389; //Get Detail of this Project
+         $togglObj->getProjectDetail($id);
+         break;     
+      case 'edit_project':
+         $data = array();
+         $data['id'] = 3794389; //Project Id to update
+         $data['name'] = 'My Updated Project';
+         $togglObj->editProject($data);
+         break;    
+      case 'get_project_users':
+         $id = 3794389; //Get Users of this Projects
+         $togglObj->getProjectUsers($id);
+         break;                        
+         
+      case 'add_tag':
+         $data = array();
+         $data['wid'] = 482143;  //Workspace Id to add tag to
+         $data['name'] = 'Tag 1';
+         $togglObj->addTag($data);
+         break;
+      case 'edit_tag':
+         $data = array();
+         $data['id'] = 398417; //Tag Id to update
+         $data['name'] = 'Tag Name Updated';
+         $togglObj->editTag($data);
+         break;
+      case 'delete_tag':
+         $id = 398417;
+         $togglObj->deleteTag($id);
+         break;     
+
+      case 'get_report':
+         $id = 482143; //Workspace Id
+         $options = array(); //All the Default Options are set in the Class. You can override them here. 
+         //Pass following additional fields 
+            //client_ids
+            //project_ids
+            //tag_ids
+            //task_ids
+            //order_field
+            //user_ids
+         $options['report_type'] = 'details'; 
+         $togglObj->getReport($id, $options);
+         break;              
+                                       
+   }
+   
+  	$a = json_decode($togglObj->response, true);
+  	echo '<pre>';
+  	print_r($a);
+  	echo '</pre>';  	
+
+//Time Entries
